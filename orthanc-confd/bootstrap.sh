ed7442f80f8589abe4b415c8ec0f40aea44bbf12
derek <derek_merck@brown.edu>
2018-10-31 10:33:26 -0400

    Added self-configuring routing to orthanc-confd image

--- orthanc-confd/bootstrap.sh
+++ orthanc-confd/bootstrap.sh
@@ -1,7 +1,29 @@
 #! /bin/bash
 
-echo "Creating config file"
-confd -onetime -backend env
+echo "Creating Orthanc config file"
+confd -config-file /etc/confd/conf.d/orthanc.json.toml -onetime -backend env
+
+if [ "${ORTHANC_PG_ENABLED:false}" = "true" ]; then
+    echo "Checking Postgres db connection"
+    python /usr/local/bin/check_postgres.py \
+      -h ${ORTHANC_PG_HOST:-localhost} \
+      -p ${ORTHANC_PG_PORT:-5432} \
+      -u ${ORTHANC_PG_USER:-orthanc} \
+      -w ${ORTHANC_PG_PASSWORD:-'passw0rd!'} \
+      -d ${ORTHANC_PG_DATABASE:-orthanc}
+fi
+
+if [ "{$ORTHANC_ROUTE_ENABLED:false}" = "true" ]; then
+    echo "Setting up Orthanc routing script"
+    mkdir -p /var/lib/orthanc/scripts
+    confd -config-file /etc/confd/conf.d/routing.lua.toml -onetime -backend env
+fi
 
 echo "Starting Orthanc"
-exec Orthanc /etc/orthanc/orthanc.json
+
+if [ -z "$ORTHANC_VERBOSE" ]; then
+    exec Orthanc /etc/orthanc/orthanc.json
+else
+    exec Orthanc /etc/orthanc/orthanc.json --verbose
+fi
+
---
+++ orthanc-confd/check_postgres.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+# Check for pg database connection, create database if missing
+
+import psycopg2
+import argparse
+
+def parse_args():
+
+    p = argparse.ArgumentParser()
+    p.add_argument('-h', '--host', default='localhost')
+    p.add_argument('-p', '--port', default='5432')
+    p.add_argument('-u', '--user', default='postgres')
+    p.add_argument('-w', '--password', default='password')
+    p.add_argument('-d', '--database', default='orthanc')
+
+if __name__=="__main__":
+
+    opts = parse_args()
+    try:
+        con = psycopg2.connect(database=opts.database,
+                            user = opts.user,
+                            password = opts.password,
+                            host = opts.host,
+                            port = opts.port )
+
+        print("Opened database '{0}' successfully".format( opts.database ))
+        exit()
+
+    except Exception as e:
+        print("Failed to open db")
+        print(e)
+
+    try:
+        con = psycopg2.connect(
+                            user = opts.user,
+                            password =opts.password,
+                            host = opts.host,
+                            port = opts.port )
+        con.autocommit = True
+
+        cur = con.cursor()
+        cur.execute('CREATE DATABASE {};'.format(opts.database))
+        print("Created db")
+        exit()
+
+    except Exception as e:
+        print("Failed to create db")
+        raise(e)
