9dd9302cd67657cb281cda428cf1da3adea5a7c5
derek <derek_merck@brown.edu>
2018-11-03 18:03:22 -0400

    Updated confd with autorouting Moved wbv config to work with autorouting

--- orthanc-confd/bootstrap.sh
+++ orthanc-confd/bootstrap.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 echo "Creating Orthanc config file"
-confd -config-file /etc/confd/conf.d/orthanc.json.toml -onetime -backend env
+confd -confdir /etc/confd/orthanc -onetime -backend env
 
-if [ "${ORTHANC_PG_ENABLED:false}" = "true" ]; then
+if [ "${ORTHANC_PG_ENABLED:-false}" = "true" ]; then
     echo "Checking Postgres db connection"
     python /usr/local/bin/check_postgres.py \
-      -h ${ORTHANC_PG_HOST:-localhost} \
+      -H ${ORTHANC_PG_HOST:-localhost} \
       -p ${ORTHANC_PG_PORT:-5432} \
       -u ${ORTHANC_PG_USER:-orthanc} \
       -w ${ORTHANC_PG_PASSWORD:-'passw0rd!'} \
       -d ${ORTHANC_PG_DATABASE:-orthanc}
 fi
 
-if [ "{$ORTHANC_ROUTE_ENABLED:false}" = "true" ]; then
+if [ "${ORTHANC_ROUTE_ENABLED:-false}" = "true" ]; then
     echo "Setting up Orthanc routing script"
     mkdir -p /var/lib/orthanc/scripts
-    confd -config-file /etc/confd/conf.d/routing.lua.toml -onetime -backend env
+    confd -confdir /etc/confd/routing -onetime -backend env
 fi
 
 echo "Starting Orthanc"
